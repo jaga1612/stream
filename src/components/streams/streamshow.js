d1f8978e4e7c584bda5dbf9e4c00f112fb42e568
jaga1612 <jagadeesh.mech.022@gmail.com>
2022-07-10 13:19:24 +0530

    Stream show update

--- src/components/streams/streamshow.js
+++ src/components/streams/streamshow.js
@@ -1,37 +1,58 @@
 import React from "react";
 import { getStream } from "../../Actions";
 import { connect } from "react-redux";
-import FlvJs from "flv.js";
-// import flv from "flv.js";
-
+import flv from "flv.js";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
 
-    constructor(props){
-        super(props);
-                this.videoRef=React.createRef();
-         }
+  componentDidMount() {
+    this.props.getStream(this.props.match.params.id);
 
-    componentDidMount(){
-                this.props.getStream(this.props.match.params.id)
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
     }
-    
-    render(){
-        console.log(this.videoRef)
-        if(!this.props.stream) return <div>Loading....</div>
-        return (<div>
-            <video ref={this.videoRef} style={{width:"100%"}} controls={true}></video>
-            <h1>{this.props.stream.Tittle}</h1>
-            <p>{this.props.stream.Description}</p>
-        </div>)
-    }
-    
+
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+ 
+  }
+
+  render() {
+    console.log(this.videoRef);
+    if (!this.props.stream) return <div>Loading....</div>;
+    return (
+      <div>
+        <video
+          ref={this.videoRef}
+          style={{ width: "100%" }}
+          controls={true}
+        ></video>
+        <h1>{this.props.stream.Tittle}</h1>
+        <p>{this.props.stream.Description}</p>
+      </div>
+    );
+  }
 }
 
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
 
-
-const mapStateToProps=(state,ownProps)=>{
-return {stream:state.streams[ownProps.match.params.id]}
-}
-
-export default connect(mapStateToProps,{getStream})(StreamShow);
+export default connect(mapStateToProps, { getStream })(StreamShow);
